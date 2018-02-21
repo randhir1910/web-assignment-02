f175812e8e5466a1380303710e728e12487a9b92
Randhir <randhir.kumar@knoldus.in>
2018-02-21 16:19:55 +0530

    validation.js split into validation.js and jsondata.js

--- validate.js
+++ validate.js
@@ -81,23 +81,8 @@ function mobileValidate() {
 }
 
 
-function getInfo() {
-    $.ajax({
-        url: 'https://reqres.in/api/users/10',
-        dataType: 'json',
-        async: false,
-        type: "GET",
-        success: function (data) {
-            var row = $('<tr><td>' + data.data.id + '</td><td>' + data.data.first_name + '</td><td>' + data.data.last_name + '</td><td><img src="' + data.data.avatar  + '" height="50" width="50"></td></tr>');
-            $('#myTable').append(row);
-        },
-        error: function (data) {
-            console.log(data);
-        }
-    });
-}
 
-getInfo();
+
 
 // function getInfo() {
 //     $.ajax({
