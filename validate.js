1dd3e868215ea64ab6f4aad59a1edfb9ece547db
Randhir <randhir.kumar@knoldus.in>
2018-02-21 15:22:44 +0530

    image url replace by original image

--- validate.js
+++ validate.js
@@ -88,7 +88,7 @@ function getInfo() {
         async: false,
         type: "GET",
         success: function (data) {
-            var row = $('<tr><td>' + data.data.id + '</td><td>' + data.data.first_name + '</td><td>' + data.data.last_name + '</td><td>' + data.data.avatar + '</td></tr>');
+            var row = $('<tr><td>' + data.data.id + '</td><td>' + data.data.first_name + '</td><td>' + data.data.last_name + '</td><td><img src="' + data.data.avatar  + '" height="50" width="50"></td></tr>');
             $('#myTable').append(row);
         },
         error: function (data) {
